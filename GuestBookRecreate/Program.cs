9ab572303e17047db55674521dca5b29dac904b7
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-20 20:14:36 +0800

    seperate ui code from business logic , process , data get out etfc ise clas library name space obejct , list list type i ok why i can;t make stat

---
+++ GuestBookRecreate/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+// Guest book
+// Ask guest for their names, first name , last name.
+// Ask what message to the host
+// Ask if they want to add more.
+// Print guestt list and host message
+
+using GuestBookRecreateLibrary.Models;
+
+ 
+List<GuestModel> guests = new List<GuestModel>();
+GetGuestInformation(guests);
+PrintGuestINfo(guests);
+
+
+//do
+//{
+//    GuestModel guest = new GuestModel();
+//    string addMoreGuest = "";
+
+//    guest.FirstName = GetMessageFromConsole("What is your first name: ");
+//    guest.LastName = GetMessageFromConsole("What is your last name: ");
+//    guest.MessageToHost = GetMessageFromConsole("What is your message to the host: ");
+//    addMoreGuest = GetMessageFromConsole("Do you want to add more guest (yes/no)?: ");
+//    guests.Add(guest);
+
+//    Console.Clear();
+
+//} while (addMoreGuest.ToLower() == "yes");
+
+static void GetGuestInformation(List<GuestModel> guests)
+{
+    string moreGuestsComing = "";
+    do
+    {
+        GuestModel guest = new GuestModel();
+
+        guest.FirstName = GetMessageFromConsole("What is your first name: ");
+        guest.LastName = GetMessageFromConsole("What is your last name: ");
+        guest.MessageToHost = GetMessageFromConsole(message: "What message would you like to tell your host: ");
+        moreGuestsComing = GetMessageFromConsole("Are more guest coming (yes/no)?: ");
+
+        guests.Add(guest);
+
+        Console.Clear();
+    } while (moreGuestsComing.ToLower() == "yes");
+}
+
+
+static string GetMessageFromConsole(string message)
+{
+    Console.WriteLine(message);
+	string answer = Console.ReadLine();
+
+	return answer;
+}
+
+static void PrintGuestINfo(List<GuestModel> guests)
+{
+    foreach (GuestModel guest in guests)
+    {
+        Console.WriteLine(guest.GetGuestInfo);
+    }
+}
---
+++ GuestBookRecreateLibrary/Models/GuestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookRecreateLibrary.Models
+{
+    public class GuestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MessageToHost { get; set; }
+
+        public string GetGuestInfo
+        {
+            get
+            {
+                return $"{FirstName} {LastName}: {MessageToHost}";
+            }
+        }
+    }
+}
